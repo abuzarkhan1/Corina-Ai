3983a38ea3ae901062b79ad908b44465d0caeaa2
Abuzar Khan <abuzarkhan1242@gmail.com>
2024-06-28 18:39:04 +0500

    corina

---
+++ next.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ucarecdn.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'wordpress-1290326-4681702.cloudwaysapps.com',
+      },
+    ],
+  },
+}
+
+export default nextConfig
